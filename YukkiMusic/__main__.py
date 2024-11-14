3e2dfaed4983285dc8ef92a30d17b97d021dff3a
teast <daumof434@hotmail.com>
2024-11-14 10:43:07 +0200

    Update __main__.py
    Signed-off-by: teast <daumof434@hotmail.com>

--- YukkiMusic/__main__.py
+++ YukkiMusic/__main__.py
@@ -49,16 +49,7 @@ async def init():
     await userbot.start()
     await Yukki.start()
     LOGGER("YukkiMusic").info("Assistant Started Sucessfully")
-    try:
-        await Yukki.stream_call(
-            "http://docs.evostream.com/sample_content/assets/sintel1m720p.mp4"
-        )
-    except NoActiveGroupCall:
-        LOGGER("YukkiMusic").error(
-            "Please ensure the voice call in your log group is active."
-        )
-        sys.exit()
-
+ 
     await Yukki.decorators()
     LOGGER("YukkiMusic").info("YukkiMusic Started Successfully")
     await idle()
